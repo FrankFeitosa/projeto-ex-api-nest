75b37c8c31507ec51815fdaf02d5509734fb7738
FrankFeitosa <feitosfrank22@gmail.com>
2025-07-08 12:03:08 -0300

    feat: criação de diretorios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule, DatabaseModule],
+  controllers: [AppController, AuthController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
